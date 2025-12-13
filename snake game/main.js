e512702d5780ca93db8b77a3574333cdc68346d1
zhoudadudu <844429897@qq.com>
2025-12-13 13:09:31 +0800

    分离js脚本

---
+++ snake game/main.js
@@ -0,0 +1,182 @@
+
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var gameOver = false;
+
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = cols * blockSize;
+canvas.height = rows * blockSize;
+
+class Snake {
+
+    constructor({ position }) {
+        this.position = position;
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        //蛇身初始化
+        this.body = [[position.x, position.y]]
+    }
+
+    draw() {
+        context.fillStyle = "lime";
+        //画蛇头
+        context.fillRect(this.position.x, this.position.y, blockSize, blockSize);
+
+        //画蛇身
+        for (let i = 0; i < this.body.length; i++) {
+            context.fillRect(this.body[i][0], this.body[i][1], blockSize, blockSize);
+        }
+    }
+
+    update() {
+
+        this.draw();
+
+        //蛇撞墙
+        if (this.position.x < 0 || this.position.x >= cols * blockSize || this.position.y < 0 || this.position.y >= rows * blockSize) {
+            gameOver = true
+            alert("Game Over")
+        }
+
+        //蛇移动
+        this.position.x += this.velocity.x * blockSize
+        this.position.y += this.velocity.y * blockSize
+
+        //蛇撞自己
+        if (this.body.length > 1) {
+            for (let i = 1; i < this.body.length; i++) {
+                if (snake.position.x == this.body[i][0] && snake.position.y == this.body[i][1]) {
+                    gameOver = true
+                    alert("Game Over")
+                    console.log("撞自己")
+                }
+            }
+        }
+
+        //蛇吃食物
+        if (this.position.x == food.x && this.position.y == food.y) {
+            this.body.push([food.x, food.y])
+            food.placeFood()
+        }
+        //更新蛇身位置
+        for (let i = this.body.length - 1; i > 0; i--) {
+            this.body[i] = this.body[i - 1];
+        }
+        //更新蛇头位置
+        if (this.body.length) {
+            this.body[0] = [this.position.x, this.position.y];
+        }
+
+    }
+}
+
+class Food {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    draw() {
+        context.fillStyle = "red"
+        context.fillRect(this.x, this.y, blockSize, blockSize)
+    }
+
+    placeFood() {
+        this.x = Math.floor(Math.random() * cols) * blockSize
+        this.y = Math.floor(Math.random() * rows) * blockSize
+    }
+}
+
+addEventListener("keydown", (event) => {
+
+    if (snake.body.length == 1) {
+        switch (event.key) {
+            case 'w':
+                snake.velocity.x = 0;
+                snake.velocity.y = -1;
+                break
+
+            case 's':
+                snake.velocity.x = 0;
+                snake.velocity.y = 1;
+                break
+
+            case 'a':
+                snake.velocity.x = -1;
+                snake.velocity.y = 0;
+                break
+
+            case 'd':
+                snake.velocity.x = 1;
+                snake.velocity.y = 0;
+                break
+        }
+    }
+
+    if (snake.body.length > 1) {
+        switch (event.key) {
+            case 'w':
+                if (snake.velocity.y != 1) {
+                    snake.velocity.x = 0;
+                    snake.velocity.y = -1;
+                }
+                break
+
+            case 's':
+                if (snake.velocity.y != -1) {
+                    snake.velocity.x = 0;
+                    snake.velocity.y = 1;
+                }
+                break
+
+            case 'a':
+                if (snake.velocity.x != 1) {
+                    snake.velocity.x = -1;
+                    snake.velocity.y = 0;
+                }
+                break
+
+            case 'd':
+                if (snake.velocity.x != -1) {
+                    snake.velocity.x = 1;
+                    snake.velocity.y = 0;
+                }
+                break
+        }
+    }
+})
+//主循环
+function mainLoop() {
+
+    if (gameOver) {
+        return
+    }
+
+    context.fillStyle = "black"
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    food.draw()
+
+    snake.update()
+}
+
+//蛇位置初始化
+const snake = new Snake({
+    position: {
+        x: Math.floor(Math.random() * cols) * blockSize,
+        y: Math.floor(Math.random() * rows) * blockSize
+    }
+})
+
+//食物位置初始化
+const food = new Food(Math.floor(Math.random() * cols) * blockSize, Math.floor(Math.random() * rows))
+
+food.placeFood()
+
+setInterval(mainLoop, 100);
