3799c9bd724a3aee22f43f65c304f5e0103ebaf7
zhoudadudu <844429897@qq.com>
2025-12-13 12:38:47 +0800

    球拍忘记挪

--- pong/main.js
+++ pong/main.js
@@ -4,33 +4,7 @@ const context = canvas.getContext('2d')
 canvas.width = 768
 canvas.height = 512
 
-class Paddle {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
 
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
-
-        this.width = 10
-        this.height = 100
-    }
-
-    draw() {
-        context.fillStyle = 'white'
-        context.fillRect(this.x, this.y, this.width, this.height)
-    }
-
-    update() {
-        this.draw()
-
-        if (this.y + this.velocity.y > 0 //如果速度不为0
-            && this.y + this.height + this.velocity.y < canvas.height)//且 位置+面积+速度在画布内
-            this.y += this.velocity.y //更新位置
-    }
-}
 
 
 const paddle1 = new Paddle(10, canvas.height / 2 - 50)
